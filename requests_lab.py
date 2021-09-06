096258f18ff586dedc07dc8d344ea34c52a9d098
Ben Wang <xiaoyu.w.99@gmail.com>
2021-09-06 17:27:56 -0600

    update requests_lab.py to print source code

--- requests_lab.py
+++ requests_lab.py
@@ -1,7 +1,7 @@
 import requests
 
 def main():
-    print(requests.get("https://www.google.com").text)
+    print(requests.get("https://raw.githubusercontent.com/Xiaoyu-Ben-Wang/404-lab-1/main/requests_lab.py").text)
 
 if __name__=='__main__':
     main()
